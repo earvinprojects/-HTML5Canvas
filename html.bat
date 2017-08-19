e03ebad086adaea82694dc0f86da518eca8974f5
earvinlin <lin.earvin@gmail.com>
2017-08-19 14:12:46 +0800

    Add new html5 codes first time.

---
+++ html.bat
@@ -0,0 +1,6 @@
+echo "# -HTML5Canvas" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:earvinprojects/-HTML5Canvas.git
+git push -u origin master
